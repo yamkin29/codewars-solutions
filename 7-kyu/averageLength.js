58fcde0abe8648ee40b5fe6436f6a4017b3eac10
Aleksey Yamkin <me@jamkin.ru>
2025-02-10 21:31:46 +0500

    Initial commit

---
+++ 7-kyu/averageLength.js
@@ -0,0 +1,19 @@
+/*
+Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+
+    A few examples:
+
+    ['u', 'y'] =>  ['u', 'y'] // average length is 1
+    ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+    ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+If the average length is not an integer, use Math.round().
+    The input array's length > 1*/
+
+
+function averageLength(array) {
+    const average = Math.round(array.reduce((acc, item) => acc + item.length, 0) / array.length);
+
+    return array.map((item) => item[0].repeat(average));
+}
+
+console.log(averageLength(['aa', 'bbb', 'cccc']))
---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Codewars solutions</h1>
+
+<img src="https://img.shields.io/github/languages/code-size/Infinity-Kim/codewars-solutions?style=for-the-badge"> <img src="https://img.shields.io/github/repo-size/Infinity-Kim/codewars-solutions?color=purple&style=for-the-badge"> 
+<img src="https://img.shields.io/github/languages/count/Infinity-Kim/codewars-solutions?color=green&style=for-the-badge"> 
+<img src="https://img.shields.io/github/languages/top/Infinity-Kim/codewars-solutions?color=orange&style=for-the-badge">
+<img src="https://img.shields.io/github/commit-activity/m/Infinity-Kim/codewars-solutions?color=lime&style=for-the-badge">
+<img src="https://img.shields.io/github/last-commit/Infinity-Kim/codewars-solutions?color=darkgreen&style=for-the-badge">
