2cbb4dc88f89bbc02d517da047495d53625ac9e5
yamkin29 <127436193+yamkin29@users.noreply.github.com>
2025-02-19 21:44:58 +0500

    Add solution - CodeHub

---
+++ 7-kyu/celebrity-baby-names/celebrity-baby-names.js
@@ -0,0 +1,36 @@
+function validName(array) {
+  if (array.length === 0) {
+    return "You must test at least one name.";
+  }
+​
+  if (array.length === 1) {
+    return "Congratulations, you can choose any name you like!";
+  }
+​
+  function getFirstLetter(name) {
+    for (let i = 0; i < name.length; i++) {
+      if (name[i].toLowerCase() !== ' ' && name[i] !== '-') {
+        return name[i].toLowerCase();
+      }
+    }
+  }
+​
+  function getLastLetter(name) {
+    for (let i = name.length - 1; i >= 0; i--) {
+      if (name[i].toLowerCase() !== ' ' && name[i] !== '-') {
+        return name[i].toLowerCase();
+      }
+    }
+  }
+​
+  for (let i = 0; i < array.length - 1; i++) {
+    let lastCharCurrent = getLastLetter(array[i]);
+    let firstCharNext = getFirstLetter(array[i + 1]);
+​
+    if (lastCharCurrent !== firstCharNext) {
+      return "Back to the drawing board, your baby names are not compatible.";
+    }
+  }
+​
+  return "Congratulations, your baby names are compatible!";
+}
