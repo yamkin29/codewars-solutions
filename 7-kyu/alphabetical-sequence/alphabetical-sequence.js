2d8095a9ca3c44f5ce9cd9d58e48b133368c6329
yamkin29 <127436193+yamkin29@users.noreply.github.com>
2025-02-21 08:29:00 +0500

    Add solution - CodeHub

---
+++ 7-kyu/alphabetical-sequence/alphabetical-sequence.js
@@ -0,0 +1,11 @@
+function alphaSeq (str) {
+  let result = [];
+  let arr = str.split('');
+  
+  arr.forEach((item) => {
+    let a = item.toUpperCase() + item.repeat(25 - (122 - item.toLowerCase().charCodeAt(0))).toLowerCase();
+    result.push(a);
+  })
+  
+  return result.sort().join(',');
+} 
